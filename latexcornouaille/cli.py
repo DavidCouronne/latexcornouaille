c261a920443f56baba3ebfa7052b4d3f78f6c307
David Couronné <couronne.david@gmail.com>
2020-01-20 18:22:59 +0100

    Bump to version 0.1.2

--- latexcornouaille/cli.py
+++ latexcornouaille/cli.py
@@ -1,13 +1,14 @@
 import codecs
+import os
 import sys
-
 
 # Liste des extensions des fichiers auxiliaires LaTeX
 liste_ext = [".cor", ".bar", ".log", ".dvi", ".bbl", ".blg",
              ".out", ".ps", ".idx", ".ilg", ".ind", ".lof", ".lot"]
 
 
-def main(rep):
+def main():
+    rep = sys.argv[1]
     for fichier in os.listdir(rep):
         try:
             complet = rep + "/" + fichier
@@ -19,4 +20,4 @@ def main(rep):
 
 
 if __name__ == '__main__':
-    main(sys.argv[1])
+    main()
